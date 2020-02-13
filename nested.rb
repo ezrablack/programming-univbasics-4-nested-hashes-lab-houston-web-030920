68c38564110f2813ab6a77d7a974ccc159a9cbf9
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-13 23:13:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,7 +78,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     programmer_hash[:new_hash] ={ }:yukihiro_matsumoto}
 end
 
 def changing_alan
