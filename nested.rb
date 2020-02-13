9f8743dff244c7370740f54044d65da6b3fc05fd
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-13 23:25:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -99,7 +99,6 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      return programmer_hash
@@ -123,5 +122,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:known_for].push("Assembly")
+  return programmer_hash
 end
