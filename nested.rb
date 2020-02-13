d569756b9a447b5bbd0f0a1f1ee289221344a54c
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-13 23:26:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -122,6 +122,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:known_for].push("Assembly")
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
   return programmer_hash
 end
